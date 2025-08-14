3da5d09025d6f922210d477f77284932da5fa22e
Authority98 <usamarity98.com>
2025-08-14 12:54:13 +0500

    Fix Sentosa delivery zone postal codes from '09' to '098', '099' for accurate island targeting

--- supabase/migrations/20250109160000_remove_distance_config.sql
+++ supabase/migrations/20250109160000_remove_distance_config.sql
@@ -31,7 +31,7 @@ INSERT INTO delivery_configurations (
     {"id": "north", "name": "North", "postalCodes": ["72", "73", "75", "76", "77", "78", "79", "80"], "fee": 50},
     {"id": "east", "name": "East", "postalCodes": ["46", "47", "48", "49", "50", "51", "52", "53", "54", "55", "56", "57"], "fee": 45},
     {"id": "west", "name": "West", "postalCodes": ["60", "61", "62", "63", "64", "65", "66", "67", "68", "69", "70", "71"], "fee": 50},
-    {"id": "sentosa", "name": "Sentosa", "postalCodes": ["09"], "fee": 80},
+    {"id": "sentosa", "name": "Sentosa", "postalCodes": ["098", "099"], "fee": 80},
     {"id": "jurong-island", "name": "Jurong Island", "postalCodes": ["62"], "fee": 120}
   ]'::jsonb,
   '[
